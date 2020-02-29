fd7751dd196a8a4820f4197c5a4effc2c1abacf8
Scott DePouw <scott.depouw@gmail.com>
2020-02-29 10:21:03 -0500

    Fixed broken code.

--- src/PotterKata/Tests/AcceptanceTest.cs
+++ src/PotterKata/Tests/AcceptanceTest.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using PotterKata.Core;
 using Xunit;
 
@@ -10,7 +11,7 @@ namespace PotterKata.Tests
         {
             const decimal expectedTotalCost = 60m;
             BookSelectionGroup[] groups = new BookSelectionGroupFactory().CreateGroups(1, 2, 2, 3, 3, 4, 4, 5, 5, 6);
-            BookSelectionGroup optimalGroup = new DiscountOptimizer().SelectOptimalGroup(groups);
+            BookSelectionGroup optimalGroup = groups.Min();
 
             decimal totalCost = optimalGroup.CalculateTotalCost();
 
